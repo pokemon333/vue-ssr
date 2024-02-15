e61515fb6418320c9d17882017a633fb1e616318
myatkyawthu <>
2024-02-15 15:28:27 +0630

    .

--- server.js
+++ server.js
@@ -19,42 +19,38 @@ app.get('/test',(req,res)=>{
     res.send('Testing guys');
 })
 
+
 app.get('/cars/:id', (req, res) => {
     try {
         const fileContent = readFileSync(`${productionDir}/index.html`, 'utf8');
         
-
         const $ = cheerio.load(fileContent);
         
-
-        // Modify title
+        
         $('title').text('detail');
 
-        // Modify meta tags
-        $('meta[name="description"]').remove(); // Remove existing description meta tag
-        $('head').append('<meta name="description" content="detail">'); // Add new description meta tag
         
-        $('meta[property="og:type"]').remove(); // Remove existing og:type meta tag
-        $('head').append('<meta property="og:type" content="website">'); // Add new og:type meta tag
+        $('meta[name="description"]').remove(); 
+        $('head').append('<meta name="description" content="detail">');
         
-        $('meta[property="og:image"]').remove(); // Remove existing og:image meta tag
-        $('head').append('<meta property="og:image" content="https://images.unsplash.com/photo-1621155346337-1d19476ba7d6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGltYWdlfGVufDB8fDB8fHww">'); // Add new og:image meta tag
+        $('meta[property="og:type"]').remove(); 
+        $('head').append('<meta property="og:type" content="website">');
         
-        $('meta[property="og:url"]').remove(); // Remove existing og:url meta tag
-        $('head').append('<meta property="og:url" content="www.minshinsaw.com">'); // Add new og:url meta tag
+        $('meta[property="og:image"]').remove(); 
+        $('head').append('<meta property="og:image" content="https://images.unsplash.com/photo-1621155346337-1d19476ba7d6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGltYWdlfGVufDB8fDB8fHww">'); 
         
-        $('meta[property="twitter:title"]').remove(); // Remove existing twitter:title meta tag
-        $('head').append('<meta property="twitter:title" content="detail">'); // Add new twitter:title meta tag
+        $('meta[property="og:url"]').remove();
+        $('head').append('<meta property="og:url" content="www.minshinsaw.com">'); 
         
-        $('meta[property="twitter:description"]').remove(); // Remove existing twitter:description meta tag
-        $('head').append('<meta property="twitter:description" content="detail">'); // Add new twitter:description meta tag
+        $('meta[property="twitter:title"]').remove();
+        $('head').append('<meta property="twitter:title" content="detail">'); 
         
-        $('meta[property="twitter:image"]').remove(); // Remove existing twitter:image meta tag
-        $('head').append('<meta property="twitter:image" content="https://images.unsplash.com/photo-1621155346337-1d19476ba7d6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGltYWdlfGVufDB8fDB8fHww">'); // Add new twitter:image meta tag
+        $('meta[property="twitter:description"]').remove(); 
+        $('head').append('<meta property="twitter:description" content="detail">'); 
         
-
+        $('meta[property="twitter:image"]').remove(); 
+        $('head').append('<meta property="twitter:image" content="https://images.unsplash.com/photo-1621155346337-1d19476ba7d6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGltYWdlfGVufDB8fDB8fHww">'); 
         
-
         res.send($.html());
     } catch (error) {
         console.error('Error reading file:', error);
@@ -66,6 +62,7 @@ app.get('/cars/:id', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile('index.html', { root: path.resolve(__dirname, productionDir) });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
